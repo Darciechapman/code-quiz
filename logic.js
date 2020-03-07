2fd8468ba4bb5323504244ca5f03fde9e2c781cd
Darciechapman <darciechapman96@gmail.com>
2020-03-07 15:31:48 +0800

    something went wrong, had to delete previous and copy into new file

--- logic.js
+++ logic.js
@@ -1,21 +1,112 @@
-var counter = 75;
 var startBtn = document.getElementById("start");
 var timer = document.getElementById("count");
-var questions1 = document.getElementById("question1");
-var questions2 = document.getElementById("question2");
-var questions3 = document.getElementById("question3");
-var questions4 = document.getElementById("question4");
-    
+var questionHeaderId = document.getElementById("question-header");
+var questionsEl = document.getElementById("questions");
+var optionsEl = document.getElementById("options");
+var markEl = document.getElementById("mark");
+
+var i = 0;
+var score = 0;
+var counter = 75;
+
+//arry of object
+//goal: to conver irtems inside array o
+const questionsString = [
+{ 
+  question: "Is henry Viii cute?",
+  options: ["yes", "no", "maybe", "so"],
+  answer: "yes"
+}, 
+{ 
+  question: "second question",
+  options: ["yes", "no", "maybe", "so"],
+  answer: "yes"
+}, 
+{ 
+  question: "third",
+  options: ["yes", "no", "maybe", "so"],
+  answer: "yes"
+}, 
+{ 
+  question: "forth",
+  options: ["yes", "no", "maybe", "so"],
+  answer: "yes"
+}]
+   
 
 function startQuiz() {
 
-    timerId = setInterval(tikTok, 1000);
+  timerId = setInterval(tikTok, 1000);
+    
+  timer.textContent = counter;
+
+  startBtn.style.display = "none";
+
+  askQuestions();
+  
+}
+
+
+function askQuestions() {
+
+  //questionHeaderId.innerText = questions.question
+
+  for (var i = 0; i < questionsString.length; i++) {
+    var question1 = questionsString[i];
+    var title = question1.question
+    questionsEl.textContent = title;
+    
+  }
+
+    optionsEl.innerHTML = "";
+
+    var optionEl = document.createElement("button");
+    optionEl.setAttribute("class", "option");
+    optionEl.setAttribute("value", options);
+    
+    optionEl.textContent = i + 1 + ". " + options;
+
+    optionEl.onclick = questionClicked;
+
+    optionsEl.appendChild(optionEl);
+  };
+  
+
+
+function questionClicked() {
+
+  if (this.value !== question[i].answer) {
+
+    counter -= 15;
+
+    if (count < 0) {
+      count = 0;
+    }
     
     timer.textContent = counter;
+    markEl.textContent = "Incorrect";
 
-    displayQuestion1;
+  } else {
+    markEl.textContent = "You're right";
+  }
 
-    startBtn.style.display = "none";
+  i++;
+
+  if (i === questions.length) {
+    quizEnd();
+  } else {
+    askQuestions();
+  }
+}
+
+function quizEnd() {
+
+  clearInterval(timer);
+
+  var endScore = document.getElementById("score");
+  endScore.textContent = count;
+
+  questionsEl.setAttribute("class", "hide");
 }
 
 
@@ -30,10 +121,8 @@ function tikTok() {
     }
   }
 
-function displayQuestion1() {
-    question1.style.display = "inline";
-}
 
 startBtn.addEventListener("click", startQuiz);
+
 
 
